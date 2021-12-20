fa4faf6b499622675dc859f3d27170f34a56f063
Maarlopez <maar.lopez.02@gmail.com>
2021-12-19 22:35:52 -0300

    Create Program.cs

---
+++ TP7 (SIN TERMINAR)/Program.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP7.Decorator;
+using TP7.Iterator;
+using TP7.Strategy;
+
+namespace TP7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Ingrese una opción: ");
+                Console.WriteLine("1- Strategy");
+                Console.WriteLine("2- Iterator");
+                Console.WriteLine("3- Test");
+                Console.WriteLine("4- Factory Method");
+                Console.WriteLine("5- Observer");
+                Console.WriteLine("6- Adapter");
+                Console.WriteLine("7- Decorator");
+                Console.WriteLine("8- Proxy");
+                Console.WriteLine("9- Command");
+                Console.WriteLine("10- Composite");
+                Console.WriteLine("11- Template Method");
+                Console.WriteLine("12- Singleton");
+                Console.WriteLine("13- Chain of Responsability");
+                Console.WriteLine("25- TestConjunto con iterdor Catedra");
+
+                string option = Console.ReadLine();
+                Console.Clear();
+                switch (option)
+                {
+                    case "1":
+                        PStrategy.Run();
+                        break;
+                    case "2":
+                        PIterator.Run();
+                        break;
+                    case "3":
+                        Test.Run();
+                        break;
+                    case "4":
+                        PFactoryMethod.Run();
+                        break;
+                    case "5":
+                        PObserver.Run();
+                        break;
+                    case "6":
+                        PAdapter.Run();
+                        break;
+                    case "7":
+                        PDecorator.Run();
+                        break;
+                    case "8":
+                        PProxy.Run();
+                        break;
+                    case "9":
+                        PCommand.Run();
+                        break;
+                    case "10":
+                        PComposite.Run();
+                        break;
+                    case "11":
+                        PTemplateMethod.Run();
+                        break;
+                    case "12":
+                        PSingleton.Run();
+                        break;
+                    case "13":
+                    PChainOfResp.Run();
+                        break;
+                    case "25":
+                                TestConjuntoDiccionario.Run();
+                                break;
+                        default:
+                            Console.WriteLine("Opcion incorrecta");
+                            break;
+                    }
+            Console.WriteLine("Toque una tecla para terminar");
+            Console.ReadKey();
+        }
+    }
+}
